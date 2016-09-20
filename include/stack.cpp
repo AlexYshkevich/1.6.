484753b8f75326bef98d65a86dbedb1271d0ebb2
AlexYshkevich <bor_bor_1997@mail.ru>
2016-09-21 00:55:15 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,6 +1,6 @@
 #include "stack.h"
-#ifndef STACK_CPP
-#define STACK_CPP
+#ifndef stack.cpp
+#define stack.cpp
 
 template <typename T>
 auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
