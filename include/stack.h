a2985bfd187b67f92cb85968d1cc09958852f3b9
AlexYshkevich <bor_bor_1997@mail.ru>
2016-09-21 19:51:03 +0400

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,29 +1,21 @@
-#pragma once
-#ifndef STACK_HPP
-#define STACK_HPP
-#include <cstdlib>
 #include <iostream>
-#include <memory>
 using namespace std;
-template <typename T>
-T* copy_with_new(const T * arr, size_t count, size_t array_size);
+
 template <typename T>
 class stack
 {
 public:
-	stack();
-	size_t count() const;
-	void push(T const &);
-	void pop();
-	const T& top();
-	~stack();
-	stack(const stack & tmp);
-	stack & operator=(const stack &obj);
-	bool operator==(stack const & rhs);
+	stack();/*noexcept*/
+	stack(const stack&); /*strong*/
+	auto count() const noexcept->size_t;/*noexcept*/
+	auto push(T const &)->void;/*strong*/
+	auto pop()->T;/*strong*/
+	auto top() const->T&;/*strong*/
+	~stack(); 	/*noexcept*/
+	auto operator=(const stack&tmp)->stack&;/*strong*/
+	auto empty()->bool;	/*noexcept*/
 private:
+	T * array_;
 	size_t array_size_;
 	size_t count_;
-	T * array_;
 };
-#include "Stack.cpp"
-#endif
