4472f6e36a82c4116012b7dc200e4a9031d79d7f
AlexYshkevich <bor_bor_1997@mail.ru>
2016-09-21 19:06:43 +0400

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,19 +1,19 @@
-#pragma once
 #include <iostream>
-
 using namespace std;
+
 template <typename T>
 class stack
 {
 public:
 	stack();
-	stack(const stack&);
+	stack(const stack&); 
 	auto count() const noexcept->size_t;
 	auto push(T const &)->void;
 	auto pop()->T;
 	auto top() const->T&;
 	~stack();
 	auto operator=(const stack&tmp)->stack&;
+	auto empty()->bool;
 private:
 	T * array_;
 	size_t array_size_;
