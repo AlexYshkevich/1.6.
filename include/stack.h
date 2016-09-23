c981fa6907cea131c4c7ad0cba0508411a2f43f8
AlexYshkevich <bor_bor_1997@mail.ru>
2016-09-23 22:59:25 +0400

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -5,14 +5,14 @@ template <typename T>
 class stack
 {
 public:
-	stack();/*noexcept*/
-	stack(const stack&); /*strong*/
-	auto count() const noexcept->size_t;/*noexcept*/
-	auto push(T const &)->void;/*strong*/
-	auto pop()->T;/*strong*/
-	~stack(); 	/*noexcept*/
-	auto operator=(const stack&tmp)->stack&;/*strong*/
-	auto empty()->bool;	/*noexcept*/
+	stack();
+ 	stack(const stack&); 
+ 	auto count() const noexcept->size_t;
+ 	auto push(T const &)->void;
+ 	auto pop()->T;
+ 	~stack(); 
+ 	auto operator=(const stack&tmp)->stack&;
+ 	auto empty()->bool;
 private:
 	T * array_;
 	size_t array_size_;
