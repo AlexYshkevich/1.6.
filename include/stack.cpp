8ebb39416e4487832fa108e6d9ea464688d6e935
AlexYshkevich <bor_bor_1997@mail.ru>
2016-09-26 20:27:56 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,6 +1,19 @@
 #include "stack.h"
 
 template <typename T>
+auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
+	T *mass = new T[array_size_m];
+	copy(tmp,tmp+count_m,mass);
+	return mass; 
+}
+
+template<typename T>
+inline auto stack<T>::empty()->bool { 
+	if (this->count()) { return false; }
+	else { return true; }
+}
+
+template <typename T>
 inline stack<T>::~stack()
 {
 	delete[] array_;
@@ -11,28 +24,23 @@ inline stack<T>::stack() :count_(0),array_size_(0),array_(nullptr){};
 
 template <typename T>
 inline auto stack<T>::push(T const &val)->void {
-	if (array_ == nullptr) {
-		array_ = new T[1];
-		array_[0] = val;
-		count_++; array_size_++;
-	}
-	else {
-		if (count_ == array_size_) {
-			T *tmp = array_;
-			array_size_ *= 2;
-			array_ = new T[array_size_];
-			std::copy(tmp,tmp+count_,array_);
-			delete[] tmp;
+	if (count_ == array_size_) {
+			size_t size = array_size_ * 2+ (array_size_==0);
+			T *tmp = mem_copy(count_,size,array_);
+			delete[] array_;
+			array_ = tmp;
+			array_size_=size;
+
 		}
-		array_[count_] = val;
 		count_++;
+		array_[count_-1] = val;
+		
+	
 	}
-}
+
+
 template <typename T>
-inline stack<T>::stack(const stack&tmp):count_(tmp.count_),array_size_(tmp.array_size_),array_(new T[tmp.array_size_]) {
-	array_ = new T[array_size_];
-	copy(tmp.array_, tmp.array_ + count_, array_);
-}
+inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(mem_copy(tmp.count_, tmp.array_size_, tmp.array_)) {}
 
 template <typename T>
 inline auto stack<T>::operator=(const stack&tmp)->stack& {
@@ -40,15 +48,9 @@ inline auto stack<T>::operator=(const stack&tmp)->stack& {
 		delete[] array_;
 		count_ = tmp.count_;
 		array_size_ = tmp.array_size_;
-		array_ = new T[array_size_];
-		copy(tmp.array_, tmp.array_ + count_, array_);
+		array_ =mem_copy(tmp.count_, tmp.array_size_, tmp.array_);
 	}
 	return *this;
-}
-
-template <typename T>
-inline auto stack<T>::print()->void {
-	for (size_t i(0); i < count_; i++) cout << array_[i] << endl; 
 }
 
 template <typename T>
