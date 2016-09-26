a948c075af0e479f877724a2a71d3346650e1096
AlexYshkevich <bor_bor_1997@mail.ru>
2016-09-26 21:20:06 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,28 +1,28 @@
 #include "stack.h"
 
-template <typename T>
+template <typename T>//копирование и выделение памяти 
 auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
 	T *mass = new T[array_size_m];
 	copy(tmp,tmp+count_m,mass);
 	return mass; 
 }
 
-template<typename T>
+template<typename T>//пуст ли стэк или нет
 inline auto stack<T>::empty()->bool { 
 	if (this->count()) { return false; }
 	else { return true; }
 }
 
-template <typename T>
+template <typename T>//освобождение памяти
 inline stack<T>::~stack()
 {
 	delete[] array_;
 }
 
-template <typename T>
+template <typename T>//конструктор по умолчанию
 inline stack<T>::stack() :count_(0),array_size_(0),array_(nullptr){};
 
-template <typename T>
+template <typename T>//вставка элемента в стэк 
 inline auto stack<T>::push(T const &val)->void {
 	if (count_ == array_size_) {
 			size_t size = array_size_ * 2+ (array_size_==0);
@@ -32,16 +32,17 @@ inline auto stack<T>::push(T const &val)->void {
 			array_size_=size;
 
 		}
-		array_[count_] = val;
 		count_++;
+		array_[count_-1] = val;
+		
 	
 	}
 
 
-template <typename T>
+template <typename T>//конструктор копирования
 inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(mem_copy(tmp.count_, tmp.array_size_, tmp.array_)) {}
 
-template <typename T>
+template <typename T>//перегрузка оператора присваивания 
 inline auto stack<T>::operator=(const stack&tmp)->stack& {
 	if (this != &tmp) {
 		delete[] array_;
@@ -52,15 +53,20 @@ inline auto stack<T>::operator=(const stack&tmp)->stack& {
 	return *this;
 }
 
-template <typename T>
+template <typename T>//возвращаем count_
 inline auto stack<T>::count() const noexcept->size_t {
 	return count_;
 }
 
-template<typename T>
-inline auto stack<T>::pop() -> T {
-if (count_ == 0) {
-throw std::range_error("Empty stack");
+template <typename T>// уменьшение count_ 
+inline auto stack<T>::pop()->T {
+	if (count_ == 0) throw logic_error("Empty!");
+	return --count_;
 }
-return array_[--count_];
+
+template <typename T>//удаление элемента
+inline auto stack<T>::top() const->T&  {
+	if (count_ == 0) throw logic_error("Empty!");
+	return array_[count_];
+
 }
