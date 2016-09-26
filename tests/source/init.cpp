98f6d0e4a23cf9b707b2a8792386e6693cb59d68
AlexYshkevich <bor_bor_1997@mail.ru>
2016-09-26 21:21:20 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -13,9 +13,17 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-  REQUIRE(s.pop()>=0);
+  REQUIRE(s.pop()==0);
 }
 
+SCENARIO("top", "[top]"){
+  stack<int> s;
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  REQUIRE(s.pop()==2);
+  REQUIRE(s.top()==3);
+}
 SCENARIO("operprisv", "[operprisv]"){
   stack<int> s1;
   s1.push(1);
@@ -40,6 +48,7 @@ SCENARIO("empty", "[empty]"){
 SCENARIO("empty2", "[empty2]"){
   stack<int> s1;
   s1.push(1);
+  s1.top();
   s1.pop();
   REQUIRE(s1.empty()==true);
 }
@@ -48,6 +57,7 @@ SCENARIO("empty3", "[empty3]"){
   stack<int> s1;
   s1.push(1);
   s1.push(2);
+  s1.top();
   s1.pop();
   
   
