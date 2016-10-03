fd6a42e3e35d0a5627b747b9a3a2eb4e42dd3886
AlexYshkevich <bor_bor_1997@mail.ru>
2016-10-03 22:35:31 +0400

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -6,7 +6,7 @@ class stack
 {
 public:
 	stack();/*noexcept*/
-	stack(stack const &); /*strong*/
+	stack(const stack&); /*strong*/
 	auto count() const noexcept->size_t;/*noexcept*/
 	auto push(T const &)->void;/*strong*/
 	auto pop()->void; /*strong*/
@@ -14,7 +14,7 @@ public:
 	~stack(); 	/*noexcept*/
 	auto operator=(const stack&tmp)->stack&;/*strong*/
 	auto empty()->bool;	/*noexcept*/
-private:
+private:	
 	T * array_;
 	size_t array_size_;
 	size_t count_;
