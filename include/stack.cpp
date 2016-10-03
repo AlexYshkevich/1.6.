800c3d5165b372733260d6560aa180ee2613d4b9
AlexYshkevich <bor_bor_1997@mail.ru>
2016-10-03 22:35:06 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,28 +1,34 @@
 #include "stack.h"
 
-template <typename T>//копирование и выделение памяти 
+template <typename T>
 auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
 	T *mass = new T[array_size_m];
-	copy(tmp,tmp+count_m,mass);
+	try{
+		std::copy(tmp,tmp+count_m,mass);
+	}
+	catch(...){
+	
+		delete[] mass;
+	
+		throw;
+	}
 	return mass; 
 }
 
-template<typename T>//пуст ли стэк или нет
-inline auto stack<T>::empty()->bool { 
-	if (this->count()) { return false; }
-	else { return true; }
+template<typename T>
+inline auto stack<T>::empty()->bool {
+	return (count_ == 0);
 }
-
-template <typename T>//освобождение памяти
+template <typename T>
 inline stack<T>::~stack()
 {
 	delete[] array_;
 }
 
-template <typename T>//конструктор по умолчанию
+template <typename T>
 inline stack<T>::stack() :count_(0),array_size_(0),array_(nullptr){};
 
-template <typename T>//вставка элемента в стэк 
+template <typename T>
 inline auto stack<T>::push(T const &val)->void {
 	if (count_ == array_size_) {
 			size_t size = array_size_ * 2+ (array_size_==0);
@@ -32,41 +38,45 @@ inline auto stack<T>::push(T const &val)->void {
 			array_size_=size;
 
 		}
+		array_[count_] = val;
 		count_++;
-		array_[count_-1] = val;
-		
 	
 	}
 
 
-template <typename T>//конструктор копирования
+template <typename T>
 inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(mem_copy(tmp.count_, tmp.array_size_, tmp.array_)) {}
 
-template <typename T>//перегрузка оператора присваивания 
-inline auto stack<T>::operator=(const stack&tmp)->stack& {
+template <typename T>
+inline auto stack<T>::operator=(const stack &tmp)->stack& {
 	if (this != &tmp) {
-		delete[] array_;
-		count_ = tmp.count_;
-		array_size_ = tmp.array_size_;
-		array_ =mem_copy(tmp.count_, tmp.array_size_, tmp.array_);
+		(stack(tmp)).swap(*this);
 	}
 	return *this;
 }
 
-template <typename T>//возвращаем count_
+template<typename T>
+auto stack<T>::swap(stack & tmp) -> void {
+	std::swap(tmp.array_size_, array_size_);
+	std::swap(tmp.array_, array_);
+	std::swap(tmp.count_, count_);
+}
+
+template <typename T>
 inline auto stack<T>::count() const noexcept->size_t {
 	return count_;
 }
 
-template <typename T>// уменьшение count_ 
+
+template <typename T> 
 inline auto stack<T>::pop()->void {
-	if (count_ == 0) throw logic_error("Empty!");
-	return --count_;
+	if (count_ == 0) throw std::logic_error("Empty!");
+	 --count_;
 }
 
-template <typename T>//удаление элемента
-inline auto stack<T>::top() const->T&  {
-	if (count_ == 0) throw logic_error("Empty!");
+template <typename T>
+inline auto stack<T>::top() const->T& {
+	if (count_ == 0) throw std::logic_error("Empty!");
 	return array_[count_];
 
 }
