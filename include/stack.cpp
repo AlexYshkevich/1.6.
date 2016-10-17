2fe73107bf227396773d22bfacf98abdeb869784
AlexYshkevich <bor_bor_1997@mail.ru>
2016-10-17 22:36:42 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,89 +1,65 @@
- #include "stack.h"  
- template<typename T> // проверка на пустоту стэка 
- inline auto stack<T>::empty()->bool {
- 	return (count_ == 0);
- }
+#include "stack.h"
  
  template <typename T>//копирование и выделение памяти 
  auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
  	T *mass = new T[array_size_m];
- 	
- 	try
+ 	copy(tmp,tmp+count_m,mass);
+  	return mass; 
+  }
+  
+  template <typename T>//освобождение памяти
+  inline stack<T>::~stack()
  {
- std::copy(tmp,tmp+count_m,mass);
+ 	delete[] array_;
  }
- catch(...)
- {
- delete[] mass;
- throw;
- }
- 	return mass;
- }
- 
- 
- template <typename T>//освобождение памяти
- inline stack<T>::~stack() {
- };
  
  template <typename T>//конструктор по умолчанию
- inline stack<T>::stack() :count_(0),size_(0),ptr_(nullptr){};
- 
- 
+ inline stack<T>::stack() :count_(0),array_size_(0),array_(nullptr){};
  
  template <typename T>//вставка элемента в стэк 
  inline auto stack<T>::push(T const &val)->void {
- 	
- 		if (count_ == size_) {
- 			size_t size= size_*2+(size_==0);
- 			T *tmp = mem_copy(count_,size,ptr_);
- 			delete[] ptr_;
- 			ptr_ = tmp;
- 			size_ = size;
+ 	if (count_ == array_size_) {
+ 			size_t size = array_size_ * 2+ (array_size_==0);
+ 			T *tmp = mem_copy(count_,size,array_);
+ 			delete[] array_;
+ 			array_ = tmp;
+ 			array_size_=size;
  
  		}
- 
- 
- 		ptr_[count_] = val;
- 		++count_;
+ 		array_[count_] = val;
+ 		count_++;
+ 	
  	}
  
  
  template <typename T>//конструктор копирования
- inline stack<T>::stack(stack const &tmp) {
- 	ptr_ = mem_copy(tmp.count_, tmp.size_, tmp.ptr_);
- 	count_ = tmp.count_;
- 	
- 	size_ = tmp.size_;
- };
+ inline stack<T>::stack(const stack&tmp) :count_(tmp.count_), array_size_(tmp.array_size_), array_(mem_copy(tmp.count_, tmp.array_size_, tmp.array_)) {}
  
  template <typename T>//перегрузка оператора присваивания 
- inline auto stack<T>::operator=(const stack &tmp)->stack& {
+ inline auto stack<T>::operator=(const stack&tmp)->stack& {
  	if (this != &tmp) {
- 		(stack(tmp)).swap(*this);
+ 		delete[] array_;
+ 		count_ = tmp.count_;
+ 		array_size_ = tmp.array_size_;
+ 		array_ =mem_copy(tmp.count_, tmp.array_size_, tmp.array_);
  	}
  	return *this;
- }
- template<typename T>
- auto stack<T>::swap(stack & tmp) -> void {
- 	std::swap(tmp.size_, size_);
- 	std::swap(tmp.ptr_, ptr_);
- 	std::swap(tmp.count_, count_);
  }
  
  template <typename T>//возвращаем count_
  inline auto stack<T>::count() const noexcept->size_t {
  	return count_;
-  }
-  
-  template <typename T>// уменьшение count_ 
- inline auto stack<T>::pop()->void {
-  	if (count_ == 0) throw std::logic_error("Empty!");
- --count_;
-  }
-  
-  template <typename T>//удаление элемента
+ }
+ 
+ template <typename T>// уменьшение count_ 
+ inline auto stack<T>::pop()->T {
+ 	if (count_ == 0) throw logic_error("Empty!");
+ 	return --count_;
+ }
+ 
+ template <typename T>//удаление элемента
  inline auto stack<T>::top() const->T& {
- 	if (count_ == 0) throw std::logic_error("Empty!");
- 	return ptr_[count_];
+ 	if (count_ == 0) throw logic_error("Empty!");
+ 	return array_[count_];
  
  }
